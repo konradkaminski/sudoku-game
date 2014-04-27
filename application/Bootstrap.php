f156b53c285d5292f72c43ac0eaca6e3408c819a
kkaminski <kkaminski@EWD-KKAMINSKI1.opteam.local>
2014-04-27 10:58:54 +0200

    poczatek aplikacji: bootstrap, dodoanie obsługi js

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,36 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
+    protected function _initDoctype() {
+        $this->bootstrap('view');
+        $view = $this->getResource('view');
+        $view->doctype('HTML5');
+    }
+    
+    protected function _initAutoloader() {
+        $autoloader = Zend_Loader_Autoloader::getInstance();
+        $autoloader->registerNamespace('Sudoku_');
+        $this->_resourceLoader();
+        return $autoloader;
+    }    
+    
+    protected function _resourceLoader() {
+        $resourceLoader = new Zend_Loader_Autoloader_Resource(array(
+            'basePath'      => APPLICATION_PATH,
+            'namespace'     => '',
+            'resourceTypes' => array(
+                'model' => array(
+                    'path'      => 'models/',
+                    'namespace' => 'Model',
+                ),
+                'form' => array(
+                    'path'      => 'forms/',
+                    'namespace' => 'Form',
+                ),
+            ),
+        ));
+    }    
+    
 
 }
 
---
+++ library/Sudoku/Controller/Action.php
@@ -0,0 +1,10 @@
+<?php
+
+class Sudoku_Controller_Action extends Zend_Controller_Action {
+    
+    public function init() {
+        parent::init();
+    }
+    
+}
+
